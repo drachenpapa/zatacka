0c185facec40c24cba8cfe90a05adebaee6b02fc
Henning Steinberg <henning.steinberg@hmmh.de>
2025-05-27 10:32:36 +0200

    ci: try to fix executable action

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,7 @@ APP_NAME="Zatacka"
 MAIN_CLASS="de.drachenpapa.zatacka.ZatackaApplication"
 JAR_PATH="./target/zatacka-1.0-SNAPSHOT.jar"
 ICON_PATH="./icon/icon.png"
-
-cd ..
+DIST_DIR="./dist"
 
 echo "Building the project with Maven..."
 mvn clean package
@@ -15,43 +14,15 @@ if [ ! -f "$JAR_PATH" ]; then
     exit 1
 fi
 
-OS=$(uname -s)
+mkdir -p "$DIST_DIR"
 
-echo "Starting jpackage for ${APP_NAME} on ${OS}..."
+echo "Creating app-image..."
+jpackage --name "$APP_NAME" \
+         --input "./target" \
+         --main-jar "$(basename "$JAR_PATH")" \
+         --main-class "$MAIN_CLASS" \
+         --type "app-image" \
+         --dest "$DIST_DIR"
+#         --icon "$ICON_PATH" \
 
-if [[ "$OS" == "Linux" ]]; then
-    echo "Creating .deb for Linux..."
-    jpackage --name "$APP_NAME" \
-             --input "./target" \
-             --main-jar "$(basename "$JAR_PATH")" \
-             --main-class "$MAIN_CLASS" \
-             --type "deb" \
-             --dest "./dist"
-#             --icon "$ICON_PATH" \
-
-elif [[ "$OS" == "Darwin" ]]; then
-    echo "Creating .pkg for macOS..."
-    jpackage --name "$APP_NAME" \
-             --input "./target" \
-             --main-jar "$(basename "$JAR_PATH")" \
-             --main-class "$MAIN_CLASS" \
-             --type "pkg" \
-             --dest "./dist"
-#             --icon "$ICON_PATH" \
-
-elif [[ "$OS" == "MINGW"* || "$OS" == "CYGWIN"* || "$OS" == "MSYS_NT"* ]]; then
-    echo "Creating .exe for Windows..."
-    jpackage --name "$APP_NAME" \
-             --input "./target" \
-             --main-jar "$(basename "$JAR_PATH")" \
-             --main-class "$MAIN_CLASS" \
-             --type "exe" \
-             --dest "./dist"
-#             --icon "$ICON_PATH" \
-
-else
-    echo "Operating system is not supported!"
-    exit 1
-fi
-
-echo "jpackage completed.
+echo "Build and packaging done: $APP_NAME"
